1eb7960a2235fd98b62bf78eb79c140354c3a710
MohanaMantra2k25 <mohanamantra2k25@gmail.com>
2025-02-15 23:06:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,64 @@ document.addEventListener('mousemove', e => {
 		`
 	})
 })
+
+
+
+
+
+
+
+
+document.getElementById("schedules").addEventListener("click", function () {
+    const currentScroll = window.scrollY;
+    const targetScroll = window.innerHeight;
+
+    if (currentScroll < targetScroll - 10 || currentScroll > targetScroll + 10) {
+        // Scroll to 100vh only if not already there
+        window.scrollTo({
+            top: targetScroll,
+            behavior: "smooth"
+        });
+    }
+	document.getElementById("over_over").scrollTo({
+        top: 0, 
+        behavior: "smooth" // Smooth scrolling effect
+    });
+});
+
+
+document.getElementById("faqs").addEventListener("click", function () {
+	const currentScroll = window.scrollY;
+    const targetScroll = window.innerHeight+20;
+
+    if (currentScroll < targetScroll - 10 || currentScroll > targetScroll + 10) {
+        // Scroll to 100vh only if not already there
+        window.scrollTo({
+            top: targetScroll,
+            behavior: "smooth"
+        });
+    }
+    document.getElementById("over_over").scrollBy({
+        top: document.getElementById("over_over").scrollHeight, // Adjust the value as needed
+        behavior: "smooth"
+    });
+});
+
+
+
+
+
+$(document).ready(function() {
+	$("#chaingestyle").click(function() {
+		let box = $(".navbar");
+		let currentRight = box.css("right");
+
+		if (currentRight === "0px") {
+			box.css("right", "130%");
+		} else {
+			box.css("right", "0");
+		}
+	});
+});
+
+
